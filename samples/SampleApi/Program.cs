a7fc69f674415d46c062d89e32559938b61bbb83
Peter Nylander <peter.nylander@gmail.com>
2025-05-14 07:41:21 +0200

    Added Api and sample api

---
+++ samples/SampleApi/Program.cs
@@ -0,0 +1,31 @@
+using Azure.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/", ([FromServices] IConfiguration configuration) =>
+{
+    var config = (configuration as IConfigurationRoot)!.GetDebugView();
+    return config;
+});
+
+app.Run();
